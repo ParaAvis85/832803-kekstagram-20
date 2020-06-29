9858514e229a175e66429eb5b943bd7368387b7d
ParaAvis <Grynza_let@mail.ru>
2020-06-29 19:52:49 +0300

    модуль большого фото

---
+++ js/big-picture.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function () {
+  var bigPicture = document.querySelector('.big-picture');
+  var bigSocialComments = document.querySelector('.social__comments'); // ищем список коментариев ul
+  var bigPictureCancel = document.querySelector('.big-picture__cancel');
+  // функция открытия большой фотографии
+  // eslint-disable-next-line no-unused-vars
+  function openLargePicture(photo) {
+    bigPicture.classList.remove('hidden'); // удаляем класс скрытия большого фото
+    bigPicture.querySelector('.big-picture__img img').src = photo.url; // ставим первое фото с массива фото
+    bigPicture.querySelector('.likes-count').textContent = photo.likes; // ставим динамичесике лайки из функции создания количества случайных лайков
+    bigPicture.querySelector('.comments-count').textContent = photo.comments.length; // ставим динамические коментарии из функции создания коментариев
+
+    document.querySelector('.social__caption').textContent = photo.description; // вставляем пустое описание фотографии
+    document.querySelector('.social__comment-count').classList.add('hidden'); // скрываем счетчик коментариев
+    document.querySelector('.comments-loader').classList.add('hidden'); // скрываем загрузку дополнительных коментариев
+    document.querySelector('body').classList.add('modal-open');
+
+    bigSocialComments.innerHTML = '';
+    for (var i = 0; i < photo.comments.length; i++) {
+      renderComment(photo.comments[i]);// вызываем функцию создания разметки
+    }
+    // document.addEventListener('keydown', onEscapePress);
+    bigPictureCancel.addEventListener('click', closeLargePicture);
+  }
+
+  // Закрытие большого фото
+  function closeLargePicture() {
+    bigPicture.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    bigPictureCancel.removeEventListener('keydown', closeLargePicture);
+  }
+
+  function renderComment(moreUserComment) {
+
+    var newComment = document.createElement('li'); // создаем элемент списка "li", методом создания элемента и вносим в переменную
+    newComment.classList.add('social__comment'); // добавляем класс для созданного "li"
+    var commentAvatar = document.createElement('img'); // создаем тег "img" и добавляем в переменную методом создания элемента
+    commentAvatar.classList.add('social__picture'); // добавляем тегу "img" класс методом создания класса
+    commentAvatar.src = moreUserComment.avatar; // добавляем адресс src и саму аватарку для коментария в теге "img"
+    commentAvatar.alt = moreUserComment.name; // добавляем льтернативный текст аватарке в теге "img"
+    commentAvatar.style = 'width="35" height="35"'; // вносим размер аватарки в тег "img" инлайн стили
+    newComment.append(commentAvatar); // добавляем весь "li" с "img" в конец списка "ul"
+
+    var newTextComment = document.createElement('p'); // добавляем тег "p" в разметку элемента "li" метод создания элементов
+    newTextComment.classList.add('social__text'); // добавляем класс тегу "p"
+    newTextComment.textContent = moreUserComment.message; // вносим сам текст коментария в тег "p"
+    newComment.append(newTextComment); // добавляем коментарий в тэлемент списка "li"
+
+    return bigSocialComments.append(newComment); // возвращаем из функции в список "ul" полностью готовый "li" с "img" "p"
+  }
+
+  window.bigPicture = {
+    open: openLargePicture,
+    close: closeLargePicture
+  };
+})();
--- js/main.js
+++ js/main.js
@@ -106,49 +106,7 @@ function renderPhotos(manyPhoto) {
 
 }
 renderPhotos(manyPhotos);
-var bigPicture = document.querySelector('.big-picture');
-// функция открытия большой фотографии
-// eslint-disable-next-line no-unused-vars
-function openLargePicture(photo) {
-  bigPicture.classList.remove('hidden'); // удаляем класс скрытия большого фото
-  bigPicture.querySelector('.big-picture__img img').src = photo.url; // ставим первое фото с массива фото
-  bigPicture.querySelector('.likes-count').textContent = photo.likes; // ставим динамичесике лайки из функции создания количества случайных лайков
-  bigPicture.querySelector('.comments-count').textContent = photo.comments.length; // ставим динамические коментарии из функции создания коментариев
 
-  document.querySelector('.social__caption').textContent = photo.description; // вставляем пустое описание фотографии
-  document.querySelector('.social__comment-count').classList.add('hidden'); // скрываем счетчик коментариев
-  document.querySelector('.comments-loader').classList.add('hidden'); // скрываем загрузку дополнительных коментариев
-  document.querySelector('body').classList.add('modal-open');
-
-  bigSocialComments.innerHTML = '';
-  for (var i = 0; i < photo.comments.length; i++) {
-    renderComment(photo.comments[i]);// вызываем функцию создания разметки
-  }
-  document.addEventListener('keydown', onEscapePress);
-  bigPictureCancel.addEventListener('click', closeLargePicture);
-}
-
-var bigSocialComments = document.querySelector('.social__comments'); // ищем список коментариев ul
-
-
-function renderComment(moreUserComment) {
-
-  var newComment = document.createElement('li'); // создаем элемент списка "li", методом создания элемента и вносим в переменную
-  newComment.classList.add('social__comment'); // добавляем класс для созданного "li"
-  var commentAvatar = document.createElement('img'); // создаем тег "img" и добавляем в переменную методом создания элемента
-  commentAvatar.classList.add('social__picture'); // добавляем тегу "img" класс методом создания класса
-  commentAvatar.src = moreUserComment.avatar; // добавляем адресс src и саму аватарку для коментария в теге "img"
-  commentAvatar.alt = moreUserComment.name; // добавляем льтернативный текст аватарке в теге "img"
-  commentAvatar.style = 'width="35" height="35"'; // вносим размер аватарки в тег "img" инлайн стили
-  newComment.append(commentAvatar); // добавляем весь "li" с "img" в конец списка "ul"
-
-  var newTextComment = document.createElement('p'); // добавляем тег "p" в разметку элемента "li" метод создания элементов
-  newTextComment.classList.add('social__text'); // добавляем класс тегу "p"
-  newTextComment.textContent = moreUserComment.message; // вносим сам текст коментария в тег "p"
-  newComment.append(newTextComment); // добавляем коментарий в тэлемент списка "li"
-
-  return bigSocialComments.append(newComment); // возвращаем из функции в список "ul" полностью готовый "li" с "img" "p"
-}
 
 // черновой вариант открытия скрытия фото для редактирования
 
@@ -254,14 +212,15 @@ effectsList.addEventListener('click', function (evt) {
 
 
 // module4-3
-var bigPictureCancel = document.querySelector('.big-picture__cancel');
+
 // открытие открытие по ентеру
 var onPreviewEnterPress = function (evt) {
   if (evt.keyCode === 13) {
     var picture = evt.target.closest('.picture');
     if (picture) {
       var id = picture.dataset.id;
-      openLargePicture(manyPhotos[id]);
+      window.bigPicture.open(manyPhotos[id]);
+      document.addEventListener('keydown', onEscapePress);
     }
 
   }
@@ -271,22 +230,18 @@ function onCustomPhotoClick(evt) {
   var picture = evt.target.closest('.picture');
   if (picture) {
     var id = picture.dataset.id;
-    openLargePicture(manyPhotos[id]);
+    window.bigPicture.open(manyPhotos[id]);
+    document.addEventListener('keydown', onEscapePress);
   }
 }
 
 function onEscapePress(evt) {
   if (evt.keyCode === 27) {
-    closeLargePicture();
+    window.bigPicture.close();
+    document.removeEventListener('keydown', onEscapePress);
   }
 }
-// Закрытие большого фото
-function closeLargePicture() {
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onEscapePress);
-  bigPictureCancel.removeEventListener('keydown', closeLargePicture);
-}
+
 // обработчики для случайных фото
 photosList.addEventListener('click', onCustomPhotoClick);
 photosList.addEventListener('keydown', onPreviewEnterPress);
