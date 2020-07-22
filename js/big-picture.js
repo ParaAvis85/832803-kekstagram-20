fb95802d48525470ad5d9a56810d4d43be89f3d5
ParaAvis <Grynza_let@mail.ru>
2020-07-22 15:36:52 +0300

    приведение к единообразию модуля big-picture

--- js/big-picture.js
+++ js/big-picture.js
@@ -6,6 +6,25 @@
   var bigPictureCancel = document.querySelector('.big-picture__cancel');
   // функция открытия большой фотографии
 
+  function renderComment(moreUserComment) {
+
+    var newComment = document.createElement('li'); // создаем элемент списка "li", методом создания элемента и вносим в переменную
+    newComment.classList.add('social__comment'); // добавляем класс для созданного "li"
+    var commentAvatar = document.createElement('img'); // создаем тег "img" и добавляем в переменную методом создания элемента
+    commentAvatar.classList.add('social__picture'); // добавляем тегу "img" класс методом создания класса
+    commentAvatar.src = moreUserComment.avatar; // добавляем адресс src и саму аватарку для коментария в теге "img"
+    commentAvatar.alt = moreUserComment.name; // добавляем льтернативный текст аватарке в теге "img"
+    commentAvatar.style = 'width="35" height="35"'; // вносим размер аватарки в тег "img" инлайн стили
+    newComment.append(commentAvatar); // добавляем весь "li" с "img" в конец списка "ul"
+
+    var newTextComment = document.createElement('p'); // добавляем тег "p" в разметку элемента "li" метод создания элементов
+    newTextComment.classList.add('social__text'); // добавляем класс тегу "p"
+    newTextComment.textContent = moreUserComment.message; // вносим сам текст коментария в тег "p"
+    newComment.append(newTextComment); // добавляем коментарий в тэлемент списка "li"
+
+    return bigSocialComments.append(newComment); // возвращаем из функции в список "ul" полностью готовый "li" с "img" "p"
+  }
+
   function openLargePicture(photo) {
     bigPicture.classList.remove('hidden'); // удаляем класс скрытия большого фото
     bigPicture.querySelector('.big-picture__img img').src = photo.url; // ставим первое фото с массива фото
@@ -30,25 +49,6 @@
     bigPicture.classList.add('hidden');
     document.body.classList.remove('modal-open');
     bigPictureCancel.removeEventListener('keydown', closeLargePicture);
-  }
-
-  function renderComment(moreUserComment) {
-
-    var newComment = document.createElement('li'); // создаем элемент списка "li", методом создания элемента и вносим в переменную
-    newComment.classList.add('social__comment'); // добавляем класс для созданного "li"
-    var commentAvatar = document.createElement('img'); // создаем тег "img" и добавляем в переменную методом создания элемента
-    commentAvatar.classList.add('social__picture'); // добавляем тегу "img" класс методом создания класса
-    commentAvatar.src = moreUserComment.avatar; // добавляем адресс src и саму аватарку для коментария в теге "img"
-    commentAvatar.alt = moreUserComment.name; // добавляем льтернативный текст аватарке в теге "img"
-    commentAvatar.style = 'width="35" height="35"'; // вносим размер аватарки в тег "img" инлайн стили
-    newComment.append(commentAvatar); // добавляем весь "li" с "img" в конец списка "ul"
-
-    var newTextComment = document.createElement('p'); // добавляем тег "p" в разметку элемента "li" метод создания элементов
-    newTextComment.classList.add('social__text'); // добавляем класс тегу "p"
-    newTextComment.textContent = moreUserComment.message; // вносим сам текст коментария в тег "p"
-    newComment.append(newTextComment); // добавляем коментарий в тэлемент списка "li"
-
-    return bigSocialComments.append(newComment); // возвращаем из функции в список "ul" полностью готовый "li" с "img" "p"
   }
 
   window.bigPicture = {
